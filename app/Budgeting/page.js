b317ca5dbe948012af678f43a0736e05e926d99c
Noah Frank <noahdwfrank@gmail.com>
2025-12-09 16:24:10 -0700

    Added base layout

---
+++ app/Budgeting/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="p-8 bg-gray-50 min-h-screen">
+      <h1 className="text-4xl font-bold mb-3 text-center">Shopping List</h1>
+      <div className="max-w-xl mx-auto">
+      </div>
+    </main>
+  );
+}
