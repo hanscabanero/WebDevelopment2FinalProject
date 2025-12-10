0b45ed0f1528b4aa9715c6ad818f974f237c5164
Hans Cabanero <hanscabanero@gmail.com>
2025-12-10 12:43:20 -0700

    added aside navigation

--- app/page.js
+++ app/page.js
@@ -1,15 +1,60 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Page()
-{
+export default function Page() {
   return (
-    <main>
-      <h1>CPRG306: Web Development 2 - Assignments</h1>
-      <ul>
-        <li>
-          <Link href="/Budgeting">Go to budgeting</Link>
-        </li> 
-      </ul>
+    <main className="min-h-screen flex text-[#ededed]">
+
+      {/* Sidebar */}
+      <aside className="w-60 p-6  bg-green-950">
+        <ul className="space-y-4 text-lg">
+          <li className="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+            </svg>
+            <Link 
+              href="/Budgeting"
+              className="hover:text-gray-200 pl-5"
+            >
+              Budgeting
+            </Link>
+          </li>
+          <li className="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+            </svg>
+            <Link
+              href="/task-management"
+              className="hover:text-gray-200 pl-5"
+            >
+              Task Management
+            </Link>
+          </li>
+          <li className="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+            </svg>
+            <Link
+              href="/journaling"
+              className="hover:text-gray-200 mx-5"
+            >
+              Journaling
+            </Link>
+          </li>
+        </ul>
+      </aside>
+
+      {/* Main content area */}
+      <div className="flex-1 flex flex-col">
+
+        {/* Header */}
+        <header className="p-4 border-b bg-[#181818] text-2xl font-semibold">
+          My Dashboard
+        </header>
+
+        <section className="p-6">
+          <h1 className="text-3xl text-gray-800">Welcome</h1>
+        </section>
+      </div>
     </main>
   );
 }
