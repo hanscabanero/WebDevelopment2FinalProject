fb2762d54378b2752157e5d8d530c637229c7aec
koreanjesus101 <sean.lee@edu.sait.ca>
2025-12-10 13:30:44 -0700

    Journal page is done with API called from server not from browser
    Journal page is done, with minimal styling. Called ZenQuotes API from the server instead of browser due to CORS restrictions so created app/api/route/route.js. Journals can be edited now and will implement delete fucntion

--- app/Journaling/page.js
+++ app/Journaling/page.js
@@ -1,9 +1,56 @@
-export default function Page() {
+"use client";
+import { useState, useEffect } from "react";
+import JournalForm from "./journal-form";
+import QuoteBox from "./quote-box";
+import JournalList from "./journal-list";
+
+export default function JournalingSystem() {
+  const [entries, setEntries] = useState([]);
+  const [editingEntry, setEditingEntry] = useState(null);
+
+  // Load from localStorage
+  useEffect(() => {
+    const saved = localStorage.getItem("journalEntries");
+    if (saved) setEntries(JSON.parse(saved));
+  }, []);
+
+  // Save entries on change
+  useEffect(() => {
+    localStorage.setItem("journalEntries", JSON.stringify(entries));
+  }, [entries]);
+
+  // Add new entry
+  const addEntry = (entry) => {
+    const newEntry = { id: Date.now(), ...entry };
+    setEntries((prev) => [newEntry, ...prev]);
+  };
+
+  // Update existing entry
+  const updateEntry = (updated) => {
+    setEntries((prev) =>
+      prev.map((e) => (e.id === updated.id ? updated : e))
+    );
+    setEditingEntry(null);
+  };
+
   return (
-    <main className="p-8 bg-gray-50 min-h-screen">
-      <h1 className="text-4xl font-bold mb-3 text-center">Shopping List</h1>
-      <div className="max-w-xl mx-auto">
+    <div className="min-h-screen bg-linear-to-br from-slate-100 to-slate-200 p-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        
+        <JournalForm 
+          onAdd={addEntry} 
+          onUpdate={updateEntry}
+          editingEntry={editingEntry}
+        />
+
+        <JournalList 
+          entries={entries} 
+          onEdit={setEditingEntry}
+        />
+
+        <QuoteBox/>
+
       </div>
-    </main>
+    </div>
   );
 }
