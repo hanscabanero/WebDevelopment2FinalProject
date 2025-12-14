c55d30b2b566cbd614c802ff9c125806a36f8554
koreanjesus101 <sean.lee@edu.sait.ca>
2025-12-14 00:27:06 -0700

    Add AuthContext for authentication

---
+++ app/contexts/AuthContext.js
@@ -0,0 +1,39 @@
+"use client";
+
+import { useContext, createContext, useState, useEffect } from "react";
+import {
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  GithubAuthProvider,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const gitHubSignIn = () => {
+    const provider = new GithubAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+
+  const firebaseSignOut = async () => {
+    await signOut(auth);
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, setUser);
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, gitHubSignIn, firebaseSignOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useUserAuth = () => useContext(AuthContext);
